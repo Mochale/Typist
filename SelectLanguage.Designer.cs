445b7525a2f8e8bcd66a135064dda1b31daa7802
matio <matio.nora2000@gmail.com>
2022-12-29 16:06:56 +0330

    feat-design: select language form designed

---
+++ SelectLanguage.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Typist.App
+{
+    partial class SelectLanguage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbmLanguage = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(193, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "زبان برنامه:";
+            // 
+            // cbmLanguage
+            // 
+            this.cbmLanguage.FormattingEnabled = true;
+            this.cbmLanguage.Location = new System.Drawing.Point(12, 29);
+            this.cbmLanguage.Name = "cbmLanguage";
+            this.cbmLanguage.Size = new System.Drawing.Size(175, 24);
+            this.cbmLanguage.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 59);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 28);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "اجرا";
+            this.btnStart.UseVisualStyleBackColor = true;
+            // 
+            // SelectLanguage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 101);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.cbmLanguage);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "SelectLanguage";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "انتخاب زبان";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbmLanguage;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
---
+++ SelectLanguage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Typist.App
+{
+    public partial class SelectLanguage : Form
+    {
+        public SelectLanguage()
+        {
+            InitializeComponent();
+        }
+    }
+}
