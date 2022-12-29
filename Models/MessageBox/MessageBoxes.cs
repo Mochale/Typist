f5437fdcbbf275706c27ae28d342c30aa313a7b1
matio <matio.nora2000@gmail.com>
2022-12-29 14:36:05 +0330

    feat-model: message boxes model created

---
+++ Models/MessageBox/MessageBoxes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Typist.App.Models.MessageBox
+{
+    public abstract class MessageBoxBase
+    {
+        protected abstract MessageBoxIcon Icon { get; }
+        protected virtual MessageBoxButtons Button => MessageBoxButtons.OK;
+
+        public string Header { get; set; }
+        public string Message { get; set; }
+
+        public void Show()
+        {
+            System.Windows.Forms.MessageBox.Show(Message, Header, Button, Icon);
+        }
+
+    }
+    public class MessageBoxes
+    {
+        public Success Success { get; set; }
+        public Error Error { get; set; }
+        public FinishGame FinishGame { get; set; }
+    }
+
+    public class Success : MessageBoxBase
+    {
+        protected override MessageBoxIcon Icon => MessageBoxIcon.Information;
+    }
+
+    public class Error : MessageBoxBase
+    {
+        protected override MessageBoxIcon Icon => MessageBoxIcon.Error;
+    }
+
+    public class FinishGame : MessageBoxBase
+    {
+        protected override MessageBoxIcon Icon => MessageBoxIcon.Asterisk;
+    }
+}
