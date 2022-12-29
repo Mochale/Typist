4440b7dc3d46ab50785fd731db8defc3bd68d1f1
matio <matio.nora2000@gmail.com>
2022-12-29 14:09:29 +0330

    feat-form: main form get data from resource

--- Form1.cs
+++ Form1.cs
@@ -5,16 +5,50 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Typist.App
 {
     public partial class Form1 : Form
     {
+        protected string ResourceFile = "Forms/Main.json";
+        public string Lang { get; set; }
         public Form1()
         {
             InitializeComponent();
+
+            if (Lang == null || Lang == String.Empty)
+                Lang = "Fa";
+            string resource = File.ReadAllText($"{Application.StartupPath}/Resources/{Lang}/{ResourceFile}");
+            Model model = JsonSerializer.Deserialize<Model>(resource);
+
+            this.Text = model.Text;
+
+            btnStart.Text = model.Buttons.Start.Text;
         }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            new Game().ShowDialog();
+        }
+    }
+
+    class Model
+    {
+        public string Text { get; set; }
+        public Button Buttons { get; set; }
+    }
+
+    class Button
+    {
+        public Start Start { get; set; }
+    }
+    class Start
+    {
+        public string Text { set; get; }
     }
 }
