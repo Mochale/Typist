4001e54c4bc5ac836d84b2319e2e9bf296527a88
matio <matio.nora2000@gmail.com>
2022-12-29 16:24:08 +0330

    feat-form: main form description added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,12 @@
         private void InitializeComponent()
         {
             this.btnStart = new System.Windows.Forms.Button();
+            this.rchTextDescription = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(12, 152);
+            this.btnStart.Location = new System.Drawing.Point(12, 120);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(75, 28);
             this.btnStart.TabIndex = 0;
@@ -41,11 +42,20 @@
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
+            // rchTextDescription
+            // 
+            this.rchTextDescription.Location = new System.Drawing.Point(12, 12);
+            this.rchTextDescription.Name = "rchTextDescription";
+            this.rchTextDescription.Size = new System.Drawing.Size(348, 102);
+            this.rchTextDescription.TabIndex = 1;
+            this.rchTextDescription.Text = "";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(372, 192);
+            this.ClientSize = new System.Drawing.Size(372, 157);
+            this.Controls.Add(this.rchTextDescription);
             this.Controls.Add(this.btnStart);
             this.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4);
@@ -60,6 +70,7 @@
         #endregion
 
         private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.RichTextBox rchTextDescription;
     }
 }
 
